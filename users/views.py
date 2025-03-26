d5246bc3c9e36db1ee718f1c5f819928f1dab0db
pugalo0123 <nicglad2005@gmail.com>
2025-03-26 19:20:07 +0700

    login required decorator - redirect to tarnet page

--- users/views.py
+++ users/views.py
@@ -17,6 +17,10 @@ def login(request):
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username}, вы вошли в аккаунт')
+
+                if request.POST.get('next', None):
+                    return HttpResponseRedirect(request.POST.get('next'))
+                
                 return HttpResponseRedirect(reverse('main:index'))
     else:
         form = UserLoginForm()
@@ -45,6 +49,7 @@ def registration(request):
         'form': form
     }
     return render(request, 'users/registration.html', context)
+
 
 @login_required
 def profile(request):
