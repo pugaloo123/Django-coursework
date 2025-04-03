2c4facb4d5a7428b0185225597cddd49fd3d3daa
pugalo0123 <nicglad2005@gmail.com>
2025-04-03 22:08:14 +0700

    Session - carts for anonymous user

--- users/views.py
+++ users/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib import auth, messages
+from django.contrib import auth, messages, sessions
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect, render
 from django.urls import reverse
 
+from carts.models import Cart
 from users.form import ProfileForm, UserLoginForm, UserRegistrationForm
 
 
@@ -14,9 +15,15 @@ def login(request):
             username = request.POST['username']
             password = request.POST['password']
             user = auth.authenticate(username=username, password=password)
+
+            session_key = request.session.session_key
+
             if user:
                 auth.login(request, user)
                 messages.success(request, f'{username}, вы вошли в аккаунт')
+
+                if session_key:
+                    Cart.objects.filter(session_key=session_key).update(user=user)
 
                 redirect_page = request.POST.get('next', None)
                 if redirect_page and redirect_page != reverse('user:logout'):
@@ -38,8 +45,15 @@ def registration(request):
         form = UserRegistrationForm(data=request.POST)
         if form.is_valid():
             form.save()
+
+            session_key = request.session.session_key
+
             user = form.instance
             auth.login(request, user)
+
+            if session_key:
+                Cart.objects.filter(session_key=session_key).update(user=user)
+
             messages.success(request, f'{user.username}, вы успешно зарегестрировались и вошли в аккаунт')
             return HttpResponseRedirect(reverse('main:index'))
     else:
