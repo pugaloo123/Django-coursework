c8e7bc0d05f6e9ee08e67c0fa988b45f9291ff66
pugalo0123 <nicglad2005@gmail.com>
2025-04-07 11:08:26 +0700

    Admin panel setup

--- carts/admin.py
+++ carts/admin.py
@@ -2,4 +2,27 @@ from django.contrib import admin
 
 from carts.models import Cart
 
-admin.site.register(Cart)
+# admin.site.register(Cart)
+
+class CartTabAdmin(admin.TabularInline):
+    model = Cart
+    fields = 'product', 'quantity', 'create_timestamp'
+    search_fields = 'product', 'quantity', 'create_timestamp'
+    readonly_fields = ('create_timestamp', )
+    extra = 1
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['user_display', 'product_display', 'quantity', 'create_timestamp', ]
+    list_filter = ['create_timestamp', 'user', 'product__name', ]
+
+    def user_display(self, obj):
+        if obj.user:
+            return str(obj.user)
+        return 'Анонимный пользователь'
+
+    def product_display(self, obj):
+        return str(obj.product.name)
+
+
--- goods/models.py
+++ goods/models.py
@@ -1,6 +1,7 @@
 from email.mime import image
 from unicodedata import category
 from django.db import models
+from django.urls import reverse
 
 class Categories(models.Model):
     name = models.CharField(max_length=150, unique=True, verbose_name='Название')
@@ -32,11 +33,17 @@ class Products(models.Model):
         verbose_name_plural = 'Продукты'
         ordering = ("id",) 
 
+
     def __str__(self):
         return f'{self.name} Количество - {self.quantity}'
     
+    def get_absolute_url(self):
+        return reverse("catalog:product", kwargs={"product_slug": self.slug})
+    
+
     def display_id(self):
         return f'{self.id:05}'
+    
 
     def sell_price(self):
         if self.discount:
